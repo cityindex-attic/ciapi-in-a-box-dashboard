54403ee9a9f46e39e8816d7be748f279f5edacf9
mrdavidlaing <david@davidlaing.com>
2011-09-30 18:43:02 +0100

    Integrate JsonSchemaDocs

---
+++ src/content/index.js
@@ -0,0 +1,60 @@
+﻿/*
+configuration notes:
+
+
+* id properties must be unique thoughout the index, including any dynamically generated content i.e. smd and schema, otherwise the tree's underlying store chokes.
+
+
+
+
+*/
+
+var jsdConfig = {
+
+    staticContentBase: "content",
+    header: "<span style=''>HEADER</span>",
+    footer: "<span style=''>FOOTER</span>",
+    defaultNode: 'Getting_Started.Overview',
+
+    index: [{
+        label: 'Getting Started',
+        id: 'Getting_Started',
+        children: [{
+            label: 'Overview',
+            id: 'Getting_Started.Overview',
+            url: '../diagram.html'
+        },
+        {
+            label: 'Embedded HTML content',
+            id: 'Getting_Started.embedded',
+            content: '<h1>EMBEDDED</h1>'
+        }]
+    },
+    {
+        label: 'Examples',
+        id: 'Examples',
+        children: [{
+            label: 'Example 1',
+            id: 'Examples.Example1',
+            url: 'static/!test.htm'
+        }]
+    }],
+
+    samples: {
+        type: {
+            ApiActiveStopLimitOrderDTO: {
+                "Sample 1": "samples/types/ApiActiveStopLimitOrderDTO.htm"
+            }
+        },
+        service: {
+            CreateSession:
+            {
+                "Sample 1": "samples/services/CreateSession.htm",
+                "Sample 2": "samples/services/CreateSession.htm"
+            }
+        }
+    }
+};
+
+
+
---
+++ src/content/metadata/ssmd.js
@@ -0,0 +1,72 @@
+﻿var ciConfig=ciConfig||{};
+ciConfig.ssmd = {
+    "HTTP-SSM-Version": "0.1",
+    "version": "1",
+    "target": "http://pushpreprod.cityindextest9.co.uk/",
+    "schema": "http://ciapipreprod.cityindextest9.co.uk/TradingApi/schema?includeDemoValue=true",
+    "description": "City Index Streaming API v1",
+    "streams": {
+        "NewsHeadlines": {
+            "description": "Stream of current news headlines.  Try NEWS.MOCKHEADLINES.UK for a mock stream",
+            "endpoint": "CITYINDEXSTREAMING", 
+            "channel": "NEWS.HEADLINES.{category}", 
+            "transport": "HTTP", 
+            "protocol": "lightstreamer-3.6", 
+            "messageType": {
+                "$ref": "#.NewsDTO"
+            },
+            "group": "Streaming API",
+            "parameters": [
+                {
+                    "type": "string",
+                    "name": "category",
+                    "description": "A news category",
+                    "minLength": 1,
+                    "maxLength": 100,
+                    "demoValue": "UK"
+                }
+            ]
+        },
+        "Prices": {
+            "description": "Stream of current prices. Try PRICES.PRICE.154297 (GBP/USD (per 0.0001) CFD) which prices Mon - Fri 24hrs",
+            "endpoint": "CITYINDEXSTREAMING", 
+            "channel": "PRICES.PRICE.{marketId}", 
+            "transport": "HTTP", 
+            "protocol": "lightstreamer-3.6", 
+            "messageType": {
+                "$ref": "#.PriceDTO"
+            },
+            "group": "Streaming API",
+            "parameters": [
+               {
+                    "type": "string",
+                    "name": "marketId",
+                    "description": "The marketId",
+                    "demoValue": "71442"
+                }
+            ]
+        },
+        "ClientAccountMargin": {
+            "description": "Stream of clients current margin",
+            "endpoint": "CLIENTACCOUNT", 
+            "channel": "ALL", 
+            "transport": "HTTP", 
+            "protocol": "lightstreamer-3.6", 
+            "messageType": {
+                "$ref": "#.ClientAccountMarginDTO"
+            },
+            "group": "Streaming API"
+        },
+        "Quotes": {
+            "description": "Stream of quotes",
+            "endpoint": "TRADINGACCOUNT", 
+            "channel": "QUOTE.ALL", 
+            "transport": "HTTP", 
+            "protocol": "lightstreamer-3.6", 
+            "messageType": {
+                "$ref": "#.QuoteDTO"
+            },
+            "group": "Streaming API"
+        }
+    }
+};
